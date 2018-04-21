c5193991b67c29ccffaf6432b1cdd99b6c752946
pontus <pontus@minabastapolare.se>
2018-04-21 10:23:12 +0200

    Added shmup class, and some folders

---
+++ src/shmup/Shmup.ts
@@ -0,0 +1,25 @@
+// import 'phaser';
+export class Shmup {
+
+  player: Phaser.GameObjects.Sprite;
+
+  constructor(scene: Phaser.Scene) {
+
+  }
+
+
+  public preload() {
+
+  }
+
+  public create() {
+    // this.player // set player here
+
+  }
+
+  public update(time: number, delta: number) {
+
+
+  }
+    
+}
