ba2d899d7239631c733a763ded05e5cee9c185e5
grimpa <grim.skarsgard@gmail.com>
2018-04-21 22:53:47 +0200

    bulle

--- src/shmup/Shmup.ts
+++ src/shmup/Shmup.ts
@@ -1,45 +1,57 @@
 import { GameMap } from './GameMap';
 import { Player } from './Player';
+import { PowerUp } from './PowerUp'
 
 export class Shmup {
 
   gamemap: GameMap;
   player: any;
   velocity: number = 100;
-<<<<<<< HEAD
-  obstacles: Phaser.Physics.Arcade.Sprite[];
-=======
-  obstacles: any;
-  bulletgroup: any;
-  // this.notes = this.scene.physics.add.group();
->>>>>>> 2f50f64def4a53d8df4cdd8d58f37bb38a774cd9
+  asteroids: Phaser.Physics.Arcade.Sprite[];
+  powerUps: PowerUp[];
 
   constructor(private scene: any) {
     this.gamemap = new GameMap(this, this.scene, this.velocity);
   }
+
 
   public preload() {
     this.scene.load.spritesheet('player', 'assets/sprites/ship_spritemap.png', { frameWidth: 65, frameHeight: 75 });
   }
   
   public create() {
-    this.obstacles = [];
-    this.gamemap.create(this.obstacles);
+    this.asteroids = [];
+    this.powerUps = [];
+    this.gamemap.create(this.asteroids, this.powerUps);
     this.player = new Player({ scene: this.scene, x: 820, y: 960-50 });
-    this.bulletgroup = this.scene.physics.add.group();
 
     this.scene.physics.world.setBounds(360, 0, 1280-360, 960);
-    this.scene.physics.add.collider(this.player.sprite, this.obstacles, this.collide, null, this.scene);
+    this.scene.physics.add.collider(this.player.sprite, this.asteroids, this.crash, null, this.scene);
+    this.scene.physics.add.collider(this.player.sprite, this.powerUps, this.powerCollect, null, this.scene);
+
   }
 
-  public collide(p, o) {
-    o.destroy();
+  public crash(player, asteroid) {
+    asteroid.destroy();
     // console.log(p, o);
+  }
+
+  public powerCollect(player, powerUp) {
+    switch (powerUp) {
+      case value:
+        
+        break;
+    
+      default:
+        break;
+    }
+    powerUp.destroy();
   }
 
   public update(time: number, delta: number) {
     this.gamemap.update(time, delta);
     this.player.update();
+
   }
     
 }
