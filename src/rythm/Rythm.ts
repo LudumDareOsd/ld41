3dadc841005df85235309c22811b04e70f49f915
inspirsea <sunesson.emil@gmail.com>
2018-04-21 13:45:32 +0200

    notes

--- src/rythm/Rythm.ts
+++ src/rythm/Rythm.ts
@@ -1,24 +1,36 @@
 import { Note } from './Note';
 import { Util } from '../utils/Util';
+import { NoteType } from './NoteType';
 
 export class Rythm {
 
-    private notes: Note[] = [];
+    private notes: any;
 
     constructor(private scene: Phaser.Scene) {
-           
     }
 
     public preload() {
     }
 
     public create() {
+        this.notes = this.scene.physics.add.group();
     }
 
     public update(time: number, delta: number) {
-       this.scene.physics.add.sprite(100, 100, 'note').setVelocity(Util.getRandomInt(10, 100), Util.getRandomInt(10, 100));
+        this.createNote(Util.getRandomInt(0, 3));
+        this.checkWorldBound(this.notes.children.entries, this.scene.physics.world);
     }
 
+    private createNote(type: NoteType) {
+        this.notes.create(50 + (type * 100), 20, 'note').setVelocity(0, 100);
+    }
 
+    private checkWorldBound(children, world) {
+        for(let item of children) {
+            if(item.y > world.bounds.height) {
+                item.destroy();
+            }
+        }
+    }
 
 }
