4a03bd90515618d8bbfd10a2e01d31dea772010d
pontus <pontus@minabastapolare.se>
2018-04-21 11:04:06 +0200

    Work on Shmup and added GameMap class

--- src/shmup/Shmup.ts
+++ src/shmup/Shmup.ts
@@ -1,10 +1,16 @@
-// import 'phaser';
+import { GameMap } from './GameMap';
+import { Obstacle } from './Obstacle';
+
 export class Shmup {
 
+  gamemap: GameMap;
   player: Phaser.GameObjects.Sprite;
+  scene: Phaser.Scene;
+  velocity: number = 1.2;
 
   constructor(scene: Phaser.Scene) {
-
+    this.scene = scene;
+    this.gamemap = new GameMap(this.velocity);
   }
 
 
@@ -14,12 +20,13 @@ export class Shmup {
 
   public create() {
     // this.player // set player here
-
+    // [] sprite = this.scene.add.sprite();
   }
 
   public update(time: number, delta: number) {
-
-
+    // this.gamemap();
+    // this.Map.update(time, delta);
+    // map.get
   }
     
 }
